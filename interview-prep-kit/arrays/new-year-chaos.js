72ccb56ea29ac340f67f3335226f6ee4c91034bd
vaRUN <58069627+NOTvaRUN@users.noreply.github.com>
2022-03-03 20:00:17 +0530

    Update new-year-chaos.js

--- interview-prep-kit/arrays/new-year-chaos.js
+++ interview-prep-kit/arrays/new-year-chaos.js
@@ -4,22 +4,6 @@
  * The function accepts INTEGER_ARRAY q as parameter.
  */
 
-function minimumBribes(q) {
-    // Write your code here
-    let skippedby = 0;
-    
-    for(let [index, value]of q.entries()){
-        if(value !== (index + 1)){
-            skippedby = value - (index + 1);
 
-            console.log(skippedby)
-            if(skippedby >= 2){
-                console.log('too chaotic');
-            }
-        }
-    }
-    console.log(skippedby);
-    
-}
 
-minimumBribes([ 2, 1, 5, 3, 4 ])
+minimumBribes([2, 1, 5, 3, 4])

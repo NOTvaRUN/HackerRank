ebd61c06d95b97e9a97a8463abcd30c1f4a6e98d
vaRUN <58069627+NOTvaRUN@users.noreply.github.com>
2022-03-22 21:06:47 +0530

    Create sparse-arrays.js

---
+++ Data Structures/sparse-arrays.js
@@ -0,0 +1,25 @@
+/*
+ * Complete the 'matchingStrings' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. STRING_ARRAY strings
+ *  2. STRING_ARRAY queries
+ */
+
+function matchingStrings(strings, queries) {
+    // Write your code here
+    let obj = {};
+    for (let item of strings) {
+        (!obj[item]) ?
+            obj[item] = 1 :
+            obj[item]++;
+    }
+    let tempArr = [];
+    for (let item of queries) {
+        tempArr.push(obj[item] ?? 0);
+    }
+    console.log(tempArr)
+}
+
+matchingStrings([ 'aba', 'baba', 'aba', 'xzxb' ], [ 'aba', 'xzxb', 'ab' ])
